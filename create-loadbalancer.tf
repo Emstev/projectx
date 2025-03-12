59d86d99f3a8350f1335b3b50236af92b78af8b8
cherish Chinweze <cherishchinweze@gmail.com>
2025-03-12 22:04:43 +0000

    created a loadbalancer.tf

---
+++ create-loadbalancer.tf
@@ -0,0 +1,6 @@
+resource "aws_lb" "loadbalancer" {
+  name            = "loadbalancer"
+  subnets         = aws_subnet.public[*].id
+  security_groups = [aws_security_group.sg.id]
+  internal        = false
+}
